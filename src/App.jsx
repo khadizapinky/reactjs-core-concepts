22e11489f9c3ac1cfed96a356870f4331fb797f3
khadiza pinky <khadizapinky303@gmail.com>
2023-10-03 19:46:09 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Country from './Components/Country/Country'
+
+
+function App() {
+  return (
+    <>
+        <Country></Country>
+           
+     </>
+  )
+}
+export default App
---
+++ src/Components/Country/Country.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <div>
+            <h1>country</h1>
+            
+        </div>
+    );
+};
+
+export default Country;
